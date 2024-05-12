b99d057c53edc9b8b3768cd5b7185dd5004ab4aa
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2024-05-12 12:08:47 +0530

    Contact and pricing updated

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -29,7 +29,7 @@ const PricingList = () => {
 
           <Button
             className="w-full mb-6"
-            href={item.price ? "/pricing" : "mailto:contact@jsmastery.pro"}
+            href={item.price ? "/pricing" : "mailto:prashant.sagar.shakya@gmail.com"}
             white={!!item.price}
           >
             {item.price ? "Get started" : "Contact us"}
